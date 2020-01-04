8bf6c0ee4ceb87674e831df77e638b3a97da4b30
Manchuria-developer <wangyuhua0820@126.com>
2020-01-04 22:31:46 +0800

    SelectionSort

--- SortApp/SortApp/Program.cs
+++ SortApp/SortApp/Program.cs
@@ -7,7 +7,7 @@ namespace SortApp
         static void Main(string[] args)
         {
             int[] array = { 1, 3, 5, 6, 2 };
-            BubbleSort(array);
+            SelectionSort(array);
             foreach (var i in array)
             {
                 Console.Write(i + " ");
@@ -21,7 +21,8 @@ namespace SortApp
             for (int i = 0; i < intArray.Length; i++)
             {
                 swapped = false;
-                for (int j = 0; j < intArray.Length - 1 - i; j++)
+                for (int j = 0; j < intArray.Length - 1 - i; j++) 
+                { 
                     if (intArray[j] > intArray[j + 1])
                     {
                         temp = intArray[j];
@@ -30,8 +31,27 @@ namespace SortApp
                         if (!swapped)
                             swapped = true;
                     }
+                }
                 if (!swapped)
                     return;
+            }
+        }
+
+        static void SelectionSort(int[] intArray)
+        {
+            int i, j, min, len = intArray.Length;
+            int temp;
+            for (i = 0; i < len - 1; i++)
+            {
+                min = i;
+                for (j = i + 1; j < len; j++)
+                {
+                    if (intArray[j]< intArray[min]) 
+                        min = j;
+                }
+                temp = intArray[min];
+                intArray[min] = intArray[i];
+                intArray[i] = temp;
             }
         }
     }
