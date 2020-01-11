a4fd64b4d4979570863020396db4f4e52eacdeff
Manchuria-developer <wangyuhua0820@126.com>
2020-01-11 21:22:45 +0800

    Shell sort

--- SortApp/SortApp/Program.cs
+++ SortApp/SortApp/Program.cs
@@ -7,7 +7,7 @@ namespace SortApp
         static void Main(string[] args)
         {
             int[] array = { 1, 3, 5, 6, 2 };
-            InsertSort(array);
+            ShellSort(array);
             foreach (var i in array)
             {
                 Console.Write(i + " ");
@@ -72,6 +72,31 @@ namespace SortApp
                 }
             }
         }
+
+        static void ShellSort(int[] intArray)
+        {
+            int length = intArray.Length;
+            for (int h = length / 2; h > 0; h = h / 2)
+            {
+                for (int i = h; i < length; i++)
+                {
+                    int temp = intArray[i];
+                    if (temp.CompareTo(intArray[i - h]) < 0)
+                    {
+                        for (int j = 0; j < i; j += h)
+                        {
+                            if (temp.CompareTo(intArray[j]) < 0)
+                            {
+                                temp = intArray[j];
+                                intArray[j] = intArray[i];
+                                intArray[i] = temp;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
     }
 }
 
